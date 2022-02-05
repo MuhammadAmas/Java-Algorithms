16a20e2419000d107ec4b4fedad8e890706ded8c
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-02-06 00:34:47 +0500

    updated

--- Hello World/main.java
+++ Hello World/main.java
@@ -1,15 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        // System.out.println("Hello, World!"); 
-
-        // float number1 = 5;
-        // float number2 = 8;
-        // float result = number1 / number2;
-
-        // System.out.println(result);
-        for (int i = 0; i <5; ++i){
-            System.out.println("Number");
-        }
-        
-    }
+        System.out.println("Hello, World!"); // Prints "Hello, World!"
+    }  
 }
