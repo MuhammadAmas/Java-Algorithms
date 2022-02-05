ed735666a4caf6128306bfea5ee9d39a401f7fcc
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-02-06 00:57:48 +0500

    Updated

---
+++ Mausam class/m.java
@@ -0,0 +1,9 @@
+public class m {
+	//attributes
+    int humidity, temperature ;
+    public m(){
+        //constructor
+        this.humidity=6;
+        this.temperature=26;
+    } 
+}
---
+++ Mausam class/main.java
@@ -0,0 +1,7 @@
+class main {
+    public static void main(String[] args){
+        m weather = new m();
+        System.out.println(weather.humidity);
+        System.out.println(weather.temperature);
+    }    
+}
