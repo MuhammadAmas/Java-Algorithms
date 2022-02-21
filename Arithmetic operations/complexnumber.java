7b6c7d842a6a397735ff4b06bf51f44173ba3559
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-02-22 03:47:07 +0500

    updated

--- Arithmetic operations/complexnumber.java
+++ Arithmetic operations/complexnumber.java
@@ -1,11 +1,11 @@
-public class complexno {
+public class complexnumber {
 	int real, imaginary;
-    public complexno(){ } //Default constructor
-    public complexno (int r, int i){ //Constructor with parameters
+    public complexnumber(){ } //Default constructor
+    public complexnumber (int r, int i){ //Constructor with parameters
         this.real = r;
         this.imaginary = i;
     }
-    public complexno(complexno a){ //copy constructor
+    public complexnumber(complexnumber a){ //copy constructor
         real = a.real;
         imaginary = a.imaginary;
     }
@@ -13,30 +13,30 @@ public class complexno {
     {
         System.out.println(+real+"+"+imaginary+"i");
     }
-    complexno addition(complexno a){ //addition of two complex numbers
-         complexno ad = new complexno();
+    complexnumber addition(complexnumber a){ //addition of two complex numbers
+         complexnumber ad = new complexnumber();
          ad.real = this.real + a.real;
          ad.imaginary = this.imaginary + a.imaginary;
          return (ad);
     }
-    complexno subtract(complexno a){ //subtraction of two numbers
-        complexno ad = new complexno();
+    complexnumber subtract(complexnumber a){ //subtraction of two numbers
+        complexnumber ad = new complexnumber();
         ad.real = this.real - a.real;
         ad.imaginary = this.imaginary - a.imaginary;
         return (ad);
    }
-   complexno multiply(complexno a){ //multiplication of two numbers
-    complexno ad = new complexno();
+   complexnumber multiply(complexnumber a){ //multiplication of two numbers
+    complexnumber ad = new complexnumber();
     ad.real = this.real * a.real;
     ad.imaginary = this.imaginary * a.imaginary;
     return (ad);
 }
     public static void main(String[] args){
-        complexno obj = new complexno(); //invoking null construtor
-        complexno obj1= new complexno(5,6); //invoking parametric construtor
-        complexno obj2 = new complexno(3,4);
-        complexno obj3 = new complexno(obj2); //invoking copy constructor
-        complexno obj4 = new complexno();
+        complexnumber obj = new complexnumber(); //invoking null construtor
+        complexnumber obj1= new complexnumber(5,6); //invoking parametric construtor
+        complexnumber obj2 = new complexnumber(3,4);
+        complexnumber obj3 = new complexnumber(obj2); //invoking copy constructor
+        complexnumber obj4 = new complexnumber();
 
         obj4 = obj1.addition(obj2); //invoking addition method
         obj4.display(); //displaying result
